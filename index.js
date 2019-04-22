2314ca20c5cc317b52def070102d068626f2f8e5
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-04-22 13:00:33 -0300

    G4 - Variaveis e seletores

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var nome = document.querySelector("#exampleInputName");
-var generoMasculino = document.querySelector("#exampleInputGenderM");
-var generoFeminino = document.querySelector("#exampleInputGenderF");
-var nascimento = document.querySelector("#exampleInputBirth");
-var pais = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail1");
-var senha = document.querySelector("#exampleInputPassword1");
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelector("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin");
