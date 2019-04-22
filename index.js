0617997c5cf1bc569605849d69f582e6a2e47c6d
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-04-22 12:48:34 -0300

    G3 - Variaveis

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-alert("Hello, World!");
+var nome = document.querySelector("#exampleInputName");
+var generoMasculino = document.querySelector("#exampleInputGenderM");
+var generoFeminino = document.querySelector("#exampleInputGenderF");
+var nascimento = document.querySelector("#exampleInputBirth");
+var pais = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail1");
+var senha = document.querySelector("#exampleInputPassword1");
