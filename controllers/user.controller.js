731f2aced08fb5006383947ee2494433793f43be
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-04-26 12:44:06 -0300

    G15 - Ajustando a View do Projeto - Operador Ternario

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -34,7 +34,12 @@ class UserController {
             fileReader.onerror = (e) => {
                 reject(e);
             }
-            fileReader.readAsDataURL(file);
+            if(file) {
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve("dist/img/boxed-bg.jpg");
+            }
+            
         });
     }
 
@@ -45,6 +50,8 @@ class UserController {
                 if(field.checked){
                     user[field.name] = field.value;
                 }
+            } else if(field.name === "admin"){
+                user[field.name] = field.checked;
             } else {
                 user[field.name] = field.value;
             }
@@ -63,19 +70,20 @@ class UserController {
     }
 
     addLine(dataUser){
-        this.tableEl.innerHTML = 
-        `<tr>
-            <td>
+        let tr = document.createElement("tr");
+        tr.innerHTML = 
+        `<td>
             <img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm">
-            </td>
-            <td>${dataUser.name}</td>
-            <td>${dataUser.email}</td>
-            <td>${dataUser.admin}</td>
-            <td>${dataUser.birth}</td>
-            <td>
+        </td>
+        <td>${dataUser.name}</td>
+        <td>${dataUser.email}</td>
+        <td>${(dataUser.admin) ? "Sim": "Não"}</td>
+        <td>${dataUser.birth}</td>
+        <td>
             <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
             <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-            </td>
-        </tr>`;
+        </td>`; 
+        this.tableEl.appendChild(tr); 
+        
     }
 }
