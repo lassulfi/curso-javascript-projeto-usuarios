44ff7155cfc3d47184189e8be1706b6da57bc703
Luis Daniel Assulfi <lassulfi@gmail.com>
2019-05-01 14:19:44 -0300

    G28 - Armazenando dados com localStorage

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -150,15 +150,16 @@ class UserController {
         );
     }
 
-    getUsersFromSessionStorage() {
+    getUsersFromStorage() {
         let users = [];
-        if(sessionStorage.getItem("users")) users = JSON.parse(sessionStorage.getItem("users"));
+        //if(sessionStorage.getItem("users")) users = JSON.parse(sessionStorage.getItem("users"));
+        if(localStorage.getItem("users")) users = JSON.parse(localStorage.getItem("users"));
 
         return users;
     }
 
     selectAll(){
-        let users = this.getUsersFromSessionStorage();
+        let users = this.getUsersFromStorage();
         users.forEach(obj => {
             let user = new User();
             user.loadFromJSON(obj);
@@ -167,11 +168,12 @@ class UserController {
     }
     
     insert(data){
-        let users = this.getUsersFromSessionStorage();
+        let users = this.getUsersFromStorage();
 
         users.push(data);
 
-        sessionStorage.setItem("users", JSON.stringify(users));
+        //sessionStorage.setItem("users", JSON.stringify(users));
+        localStorage.setItem("users", JSON.stringify(users));
     }
 
     addLine(dataUser){
